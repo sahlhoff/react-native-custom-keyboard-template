8dfcb1c7e76be74cede16b3aab5935f8442ff12e
Chad Sahlhoff <chad@sahlhoff.com>
2016-05-25 14:17:30 -0700

    initial commit

---
+++ ios/RNKeyboard/KeyboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardViewController.h
+//  RNKeyboard
+//
+//  Created by Chad Sahlhoff on 5/25/16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardViewController : UIInputViewController
+
+@end
---
+++ ios/RNKeyboard/KeyboardViewController.m
@@ -0,0 +1,78 @@
+//
+//  KeyboardViewController.m
+//  RNKeyboard
+//
+//  Created by Chad Sahlhoff on 5/25/16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import "KeyboardViewController.h"
+#import "RCTRootView.h"
+
+@interface KeyboardViewController ()
+@property (nonatomic, strong) UIButton *nextKeyboardButton;
+@end
+
+@implementation KeyboardViewController
+
+- (void)updateViewConstraints {
+    [super updateViewConstraints];
+    
+    // Add custom view sizing constraints here
+}
+
+/*
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Perform custom UI setup here
+    self.nextKeyboardButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    
+    [self.nextKeyboardButton setTitle:NSLocalizedString(@"Next Keyboard", @"Title for 'Next Keyboard' button") forState:UIControlStateNormal];
+    [self.nextKeyboardButton sizeToFit];
+    self.nextKeyboardButton.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [self.nextKeyboardButton addTarget:self action:@selector(advanceToNextInputMode) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:self.nextKeyboardButton];
+    
+    [self.nextKeyboardButton.leftAnchor constraintEqualToAnchor:self.view.leftAnchor].active = YES;
+    [self.nextKeyboardButton.bottomAnchor constraintEqualToAnchor:self.view.bottomAnchor].active = YES;
+}
+ */
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  
+  NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                      moduleName:@"RNKeyboard"
+                                               initialProperties:nil
+                                                   launchOptions:nil];
+  self.view = rootView;
+  
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated
+}
+
+- (void)textWillChange:(id<UITextInput>)textInput {
+    // The app is about to change the document's contents. Perform any preparation here.
+}
+
+- (void)textDidChange:(id<UITextInput>)textInput {
+    // The app has just changed the document's contents, the document context has been updated.
+    
+    UIColor *textColor = nil;
+    if (self.textDocumentProxy.keyboardAppearance == UIKeyboardAppearanceDark) {
+        textColor = [UIColor whiteColor];
+    } else {
+        textColor = [UIColor blackColor];
+    }
+    [self.nextKeyboardButton setTitleColor:textColor forState:UIControlStateNormal];
+}
+
+@end
